6823367e0cfec67c501f8be736001d03f991457d
shafiz <shafiz@LnxMint>
2016-12-08 19:21:13 +0600

    Added Life java file

---
+++ firstpack/src/Life.java
@@ -0,0 +1,12 @@
+
+
+public class Life {
+	int[] theSecret = getSecret();
+    int[] getSecret(){
+    	int[] x = new int[3];
+    	x[0]=1;
+    	x[1]=2;
+    	x[2]=23;
+		return x;
+	}
+}
---
+++ firstpack/src/LifeTest.java
@@ -0,0 +1,8 @@
+
+
+public class LifeTest {
+	public static void main(String args[]){
+		Life life = new Life();
+		System.out.println(life.theSecret[2]);
+	}
+}
